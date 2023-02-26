c08fecd7ec7c864a54d23a559599f7dd276fbc24
Helil Barbosa Bergária <liubergaria@hotmail.com>
2023-02-25 21:31:50 -0300

    Added a comment on preload

--- electron/main/preload.ts
+++ electron/main/preload.ts
@@ -3,6 +3,10 @@ import { contextBridge, ipcRenderer } from "electron";
 import { INativeStorage } from "@common/@types/NativeStorage";
 import { ITimeTrex } from "@common/@types/TimeTrex";
 
+// Unfortunately I couldn't find a way to build the preload script
+// separately to bundle it into one file only.
+// That's why we can't import other files here
+
 contextBridge.exposeInMainWorld("NativeStorage", {
     get: (key: string) => ipcRenderer.invoke("Storage:get", key),
     set: <T>(key: string, value: T) => ipcRenderer.invoke("Storage:set", key, value),
